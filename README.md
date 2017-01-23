08b85494e47d5abcc620978e1d6ab81711d5a54a
Dan Green <danon0404@gmail.com>
2017-01-23 21:14:25 +0700

    1.0.0

---
+++ README.md
@@ -0,0 +1,18 @@
+[![NPM](https://nodei.co/npm/htmllint-config-trigen.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/htmllint-config-trigen/)
+
+# htmllint-config-trigen
+
+Trigen's htmllint config.
+
+# Getting Started
+
+ Install with npm
+```bash
+npm i -D htmllint-config-trigen
+```
+or
+```bash
+yarn add -D htmllint-config-trigen
+```
+
+`.htmllintrc` file will be generated at project's root, also you can import config with `require('htmllint-config-trigen')`.
---
+++ postinstall.js
@@ -0,0 +1,33 @@
+/**
+ * Put `.htmllintrc` config file into project root after package install.
+ */
+
+const path     = require('path'),
+	fs         = require('fs'),
+	cwd        = process.cwd(),
+	splitedCwd = cwd.split(path.sep),
+	config     = require('./index');
+
+let projectRoot    = '',
+	htmllintrcPath = '';
+
+splitedCwd.some((part) => {
+
+	if (part == 'node_modules') {
+		return true;
+	}
+
+	projectRoot += part + path.sep;
+
+	return false;
+});
+
+htmllintrcPath = path.join(projectRoot, '.htmllintrc');
+
+fs.writeFileSync(
+	htmllintrcPath,
+	JSON.stringify(config),
+	'utf8'
+);
+
+console.log('.htmllintrc ->', htmllintrcPath, '\n');
